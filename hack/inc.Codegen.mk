0a715b06023b4a6a2600c892f84d8574e7e32910
tzununbekov <t.zununbekov@gmail.com>
2021-09-24 19:10:26 +0600

    Cleanup

--- hack/inc.Codegen.mk
+++ hack/inc.Codegen.mk
@@ -19,7 +19,8 @@ codegen: $(generators)
 
 # http://blog.jgc.org/2007/06/escaping-comma-and-space-in-gnu-make.html
 comma := ,
-space := $(null) $(null)
+space :=
+space +=
 
 deepcopy:
 	@echo "+ Generating deepcopy funcs for $(API_GROUPS)"
