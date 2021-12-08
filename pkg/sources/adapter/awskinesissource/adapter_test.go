b681d8d3523bd257768b00412d14654ec0e4097f
Antoine Cotten <hello@acotten.com>
2021-12-08 12:14:00 +0100

    Decode JSON Kinesis data in CloudEvents (#325)

--- pkg/sources/adapter/awskinesissource/adapter_test.go
+++ pkg/sources/adapter/awskinesissource/adapter_test.go
@@ -17,11 +17,13 @@ limitations under the License.
 package awskinesissource
 
 import (
+	"encoding/json"
 	"errors"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/kinesis"
@@ -121,28 +123,71 @@ func TestGetRecordsInputs(t *testing.T) {
 }
 
 func TestSendCloudevent(t *testing.T) {
-	ceClient := adaptertest.NewTestClient()
-
-	a := &adapter{
-		logger:   loggingtesting.TestLogger(t),
-		stream:   "fooStream",
-		ceClient: ceClient,
+	testCases := []struct {
+		name            string
+		eventData       []byte
+		expectEventData interface{}
+	}{
+		{
+			name:            "Data is raw bytes",
+			eventData:       []byte{'t', 'e', 's', 't'},
+			expectEventData: `"dGVzdA=="`, // base64-encoded "test"
+		},
+		{
+			name:            "Data is a JSON object",
+			eventData:       []byte(`{"test": null}`),
+			expectEventData: `{"test":null}`,
+		},
 	}
 
-	record := kinesis.Record{
-		Data:           []byte("foo"),
-		SequenceNumber: aws.String("1"),
-		PartitionKey:   aws.String("key"),
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ceClient := adaptertest.NewTestClient()
+
+			a := &adapter{
+				logger:   loggingtesting.TestLogger(t),
+				stream:   "fooStream",
+				ceClient: ceClient,
+			}
+
+			record := kinesis.Record{
+				Data:           tc.eventData,
+				SequenceNumber: aws.String("1"),
+				PartitionKey:   aws.String("key"),
+			}
+
+			err := a.sendKinesisRecord(&record)
+			assert.NoError(t, err)
+
+			gotEvents := ceClient.Sent()
+			require.Len(t, gotEvents, 1, "Expected 1 event")
+
+			eventData := make(map[string]interface{})
+			err = json.Unmarshal(gotEvents[0].Data(), &eventData)
+			require.NoError(t, err)
+
+			assert.Equal(t, "1", eventData["SequenceNumber"])
+			assert.Equal(t, "key", eventData["PartitionKey"])
+
+			// ensure the sent event has the expected encoding (base64 / raw JSON)
+			eventDataStr := stringifyEventData(t, eventData["Data"])
+			assert.Equal(t, tc.expectEventData, eventDataStr)
+		})
 	}
+}
 
-	err := a.sendKinesisRecord(&record)
-	assert.NoError(t, err)
+// stringifyEventData returns the given data as a JSON-encoded string.
+// This helps asserting the value of a Kinesis record's Data contained in a
+// CloudEvent, which can be either a JSON object encoded as a
+// map[string]interface{} (if the original record contained JSON data) or a
+// base64-encoded string (for any other type of data).
+func stringifyEventData(t *testing.T, data interface{}) string {
+	dataBytes, err := json.Marshal(data)
+	require.NoError(t, err)
 
-	gotEvents := ceClient.Sent()
-	assert.Len(t, gotEvents, 1, "Expected 1 event, got %d", len(gotEvents))
+	var jsonData json.RawMessage
+	err = json.Unmarshal(dataBytes, &jsonData)
+	require.NoError(t, err)
 
-	var gotData kinesis.Record
-	err = gotEvents[0].DataAs(&gotData)
-	assert.NoError(t, err)
-	assert.EqualValues(t, record, gotData, "Expected event %q, got %q", record, gotData)
+	return string(jsonData)
 }
