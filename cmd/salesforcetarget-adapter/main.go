b0a7dfd762a15c2a7c610847b33af5c5aa4b87c9
Pablo Mercado <odacremolbap@gmail.com>
2021-12-14 14:01:38 +0100

    move JWT library global config to main

--- cmd/salesforcetarget-adapter/main.go
+++ cmd/salesforcetarget-adapter/main.go
@@ -17,11 +17,18 @@ limitations under the License.
 package main
 
 import (
+	"github.com/golang-jwt/jwt/v4"
+
 	pkgadapter "knative.dev/eventing/pkg/adapter/v2"
 
 	"github.com/triggermesh/triggermesh/pkg/targets/adapter/salesforcetarget"
 )
 
 func main() {
+	// JWT package marshals Audience as array even if there is only one element in it. This does not
+	// seem to be supported by Salesforce. By setting the following option to false we tell the imported
+	// library to marshal single item Audience array as a string.
+	jwt.MarshalSingleStringAsArray = false
+
 	pkgadapter.Main("salesforcetarget-adapter", salesforcetarget.EnvAccessor, salesforcetarget.NewTarget)
 }
