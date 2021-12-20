83b37248b60029e9e282a529d49263c351542932
Jeffrey Naef <60796676+JeffNeff@users.noreply.github.com>
2021-12-20 15:21:16 -0700

    codegen and config updates

--- pkg/apis/sources/v1alpha1/deepcopy_generated.go
+++ pkg/apis/sources/v1alpha1/deepcopy_generated.go
@@ -3637,6 +3637,11 @@ func (in *WebhookSourceSpec) DeepCopyInto(out *WebhookSourceSpec) {
 		*out = new(ValueFromField)
 		(*in).DeepCopyInto(*out)
 	}
+	if in.CORSOrigin != nil {
+		in, out := &in.CORSOrigin, &out.CORSOrigin
+		*out = new(string)
+		**out = **in
+	}
 	return
 }
 
