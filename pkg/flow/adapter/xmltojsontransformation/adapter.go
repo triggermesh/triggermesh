4c7c93f0a376a13478bd3acc3af030f70fc8bf3f
Jeffrey Naef <60796676+JeffNeff@users.noreply.github.com>
2022-01-25 13:18:20 -0700

    Update adapter.go

--- pkg/flow/adapter/xmltojsontransformation/adapter.go
+++ pkg/flow/adapter/xmltojsontransformation/adapter.go
@@ -110,8 +110,9 @@ func (a *Adapter) dispatch(ctx context.Context, event cloudevents.Event) (*cloud
 	}
 
 	if a.sink != "" {
-		if err := a.ceClient.Send(ctx, event); err != nil {
+		if result := a.ceClient.Send(ctx, event); !cloudevents.IsACK(result) {
 			return a.replier.Error(&event, targetce.ErrorCodeAdapterProcess, err, nil)
+
 		}
 		return nil, cloudevents.ResultACK
 	}
