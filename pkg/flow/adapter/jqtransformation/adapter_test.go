b2a52e7b5ac1826378e9f364c00794b7182c5deb
Jeffrey Naef <60796676+JeffNeff@users.noreply.github.com>
2022-01-25 00:10:18 -0700

    Creates an adapter test

---
+++ pkg/flow/adapter/jqtransformation/adapter_test.go
@@ -0,0 +1,166 @@
+/*
+Copyright 2022 TriggerMesh Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package jqtransformation
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	cloudevents "github.com/cloudevents/sdk-go/v2"
+	cetest "github.com/cloudevents/sdk-go/v2/client/test"
+	"github.com/cloudevents/sdk-go/v2/protocol"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"knative.dev/eventing/pkg/adapter/v2"
+)
+
+const (
+	tCloudEventID     = "ce-abcd-0123"
+	tCloudEventType   = "ce.test.type"
+	tCloudEventSource = "ce.test.source"
+
+	tQuery        = ".foo | .."
+	tJSON         = `{"foo":"richard@triggermesh.com"}`
+	tExpectedJSON = "{\"foo\":\"richard@triggermesh.com\"}"
+
+	tFalseJSON       = "not json"
+	tExpectedFailure = "{\"Code\":\"request-parsing\",\"Description\":\"[xml] found bytes, but failed to unmarshal: EOF not json\",\"Details\":null}"
+)
+
+func TestSink(t *testing.T) {
+	testCases := map[string]struct {
+		inEvent     cloudevents.Event
+		expectEvent cloudevents.Event
+		query       string
+	}{
+		"sink ok": {
+			inEvent:     newCloudEvent(t, tJSON, cloudevents.ApplicationJSON),
+			expectEvent: newCloudEvent(t, tExpectedJSON, cloudevents.ApplicationJSON),
+			query:       tQuery,
+		},
+	}
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				body, err := ioutil.ReadAll(r.Body)
+				assert.NoError(t, err)
+				assert.Equal(t, tExpectedJSON, string(body))
+				fmt.Fprintf(w, "OK")
+			}))
+			defer svr.Close()
+
+			env := &envAccessor{
+				EnvConfig: adapter.EnvConfig{
+					Component: tCloudEventSource,
+					Sink:      svr.URL,
+				},
+				Query: tc.query,
+			}
+			ctx := context.Background()
+			c, err := cloudevents.NewClientHTTP()
+			assert.NoError(t, err)
+			a := NewAdapter(ctx, env, c)
+
+			go func() {
+				if err := a.Start(ctx); err != nil {
+					assert.FailNow(t, "could not start test adapter")
+				}
+			}()
+
+			response := sendCE(t, &tc.inEvent, c, svr.URL)
+			assert.NotEqual(t, cloudevents.IsUndelivered(response), response)
+		})
+	}
+}
+
+func TestReplier(t *testing.T) {
+	testCases := map[string]struct {
+		inEvent     cloudevents.Event
+		expectEvent cloudevents.Event
+	}{
+		"transform ok": {
+			inEvent:     newCloudEvent(t, tJSON, cloudevents.ApplicationJSON),
+			expectEvent: newCloudEvent(t, tExpectedJSON, cloudevents.ApplicationJSON),
+		},
+		"transform error": {
+			inEvent: newCloudEvent(t, tFalseJSON, cloudevents.ApplicationXML),
+
+			expectEvent: newCloudEvent(t, tExpectedFailure, cloudevents.ApplicationXML),
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			ctx := context.Background()
+
+			env := &envAccessor{
+				EnvConfig: adapter.EnvConfig{
+					Component: tCloudEventSource,
+				},
+			}
+
+			ceClient, send, responses := cetest.NewMockResponderClient(t, 1)
+
+			a := NewAdapter(ctx, env, ceClient)
+
+			go func() {
+				if err := a.Start(ctx); err != nil {
+					assert.FailNow(t, "could not start test adapter")
+				}
+			}()
+
+			send <- tc.inEvent
+
+			select {
+			case event := <-responses:
+				assert.Equal(t, tCloudEventSource, event.Event.Source())
+				assert.Equal(t, string(tc.expectEvent.DataEncoded), string(event.Event.DataEncoded))
+
+			case <-time.After(2 * time.Second):
+				assert.Fail(t, "expected cloud event response was not received")
+			}
+
+		})
+	}
+}
+
+type cloudEventOptions func(*cloudevents.Event)
+
+func newCloudEvent(t *testing.T, data, contentType string, opts ...cloudEventOptions) cloudevents.Event {
+	event := cloudevents.NewEvent()
+	event.SetID(tCloudEventID)
+	event.SetType(tCloudEventType)
+	event.SetSource(tCloudEventSource)
+	err := event.SetData(contentType, []byte(data))
+	require.NoError(t, err)
+	return event
+}
+
+func sendCE(t *testing.T, event *cloudevents.Event, cs cloudevents.Client, sink string) protocol.Result {
+	ctx := cloudevents.ContextWithTarget(context.Background(), sink)
+	c, err := cloudevents.NewClientHTTP()
+	require.NoError(t, err)
+
+	result := c.Send(ctx, *event)
+	return result
+}
