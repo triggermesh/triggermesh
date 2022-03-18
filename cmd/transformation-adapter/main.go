e281f7feccaaacbf6181757c85f9884572d1f3da
Timur Zununbekov <t.zununbekov@gmail.com>
2022-03-18 19:31:00 +0600

    Bumblebee adapter custom metrics reporter (#660)
    * Bumblebee stats reporter added

--- cmd/transformation-adapter/main.go
+++ cmd/transformation-adapter/main.go
@@ -17,50 +17,10 @@ limitations under the License.
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"log"
-
-	"github.com/kelseyhightower/envconfig"
-
-	"github.com/triggermesh/triggermesh/pkg/apis/flow/v1alpha1"
 	"github.com/triggermesh/triggermesh/pkg/flow/adapter/transformation"
+	pkgadapter "knative.dev/eventing/pkg/adapter/v2"
 )
 
-type envConfig struct {
-	// Sink URL where to send cloudevents
-	Sink string `envconfig:"K_SINK"`
-
-	// Transformation specifications
-	TransformationContext string `envconfig:"TRANSFORMATION_CONTEXT"`
-	TransformationData    string `envconfig:"TRANSFORMATION_DATA"`
-}
-
 func main() {
-	var env envConfig
-	if err := envconfig.Process("", &env); err != nil {
-		log.Fatalf("Failed to process env var: %v", err)
-	}
-
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	trnContext, trnData := []v1alpha1.Transform{}, []v1alpha1.Transform{}
-	err := json.Unmarshal([]byte(env.TransformationContext), &trnContext)
-	if err != nil {
-		log.Fatalf("Cannot unmarshal Context Transformation variable: %v", err)
-	}
-	err = json.Unmarshal([]byte(env.TransformationData), &trnData)
-	if err != nil {
-		log.Fatalf("Cannot unmarshal Data Transformation variable: %v", err)
-	}
-
-	handler, err := transformation.NewHandler(trnContext, trnData)
-	if err != nil {
-		log.Fatalf("Cannot create transformation handler: %v", err)
-	}
-
-	if err := handler.Start(ctx, env.Sink); err != nil {
-		log.Fatalf("Transformation handler: %v", err)
-	}
+	pkgadapter.Main("bumblebee", transformation.NewEnvConfig, transformation.NewAdapter)
 }
