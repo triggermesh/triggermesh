59b9e2a0d7e58bce5681398705b365c1f882da5e
Antoine Cotten <hello@acotten.com>
2022-04-21 18:14:40 +0200

    Update JiraTarget

--- pkg/targets/reconciler/jiratarget/adapter.go
+++ pkg/targets/reconciler/jiratarget/adapter.go
@@ -21,7 +21,6 @@ import (
 
 	"knative.dev/eventing/pkg/reconciler/source"
 	"knative.dev/pkg/apis"
-	"knative.dev/pkg/kmeta"
 	servingv1 "knative.dev/serving/pkg/apis/serving/v1"
 
 	commonv1alpha1 "github.com/triggermesh/triggermesh/pkg/apis/common/v1alpha1"
@@ -80,9 +79,4 @@ func makeAppEnv(o *v1alpha1.JiraTarget) []corev1.EnvVar {
 			Value: common.GetStatefulBridgeID(o),
 		},
 	}
-}
-
-// RBACOwners implements common.AdapterServiceBuilder.
-func (r *Reconciler) RBACOwners(trg commonv1alpha1.Reconcilable) ([]kmeta.OwnerRefable, error) {
-	return common.RBACOwners[*v1alpha1.JiraTarget](r.trgLister(trg.GetNamespace()))
 }
