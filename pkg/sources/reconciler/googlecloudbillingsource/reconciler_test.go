02114e3a4902804d5f2fb144aaa74cc4fa7467fd
Antoine Cotten <hello@acotten.com>
2022-04-21 16:28:10 +0200

    Update GoogleCloudBillingSource

--- pkg/sources/reconciler/googlecloudbillingsource/reconciler_test.go
+++ pkg/sources/reconciler/googlecloudbillingsource/reconciler_test.go
@@ -57,10 +57,16 @@ func reconcilerCtor(cfg *adapterConfig) Ctor {
 	return func(t *testing.T, ctx context.Context, _ *rt.TableRow, ls *Listers) controller.Reconciler {
 		r := &Reconciler{
 			cg:         staticClientGetter((*gpubsub.Client)(nil), (*gbilling.BudgetClient)(nil)),
-			srcLister:  ls.GetGoogleCloudBillingSourceLister().GoogleCloudBillingSources,
-			base:       NewTestDeploymentReconciler(ctx, ls),
 			adapterCfg: cfg,
 		}
+
+		r.base = NewTestDeploymentReconciler(
+			ctx,
+			ls,
+			func(namespace string) common.Lister[*v1alpha1.GoogleCloudBillingSource] {
+				return ls.GetGoogleCloudBillingSourceLister().GoogleCloudBillingSources(namespace)
+			},
+		)
 
 		return reconcilerv1alpha1.NewReconciler(ctx, logging.FromContext(ctx),
 			fakeinjectionclient.Get(ctx), ls.GetGoogleCloudBillingSourceLister(),
