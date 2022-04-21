5f6cc9bb197b83e71796402e797cfb470a5f1880
Antoine Cotten <hello@acotten.com>
2022-04-21 18:14:30 +0200

    Update InfraTarget

--- pkg/targets/reconciler/infratarget/adapter.go
+++ pkg/targets/reconciler/infratarget/adapter.go
@@ -23,7 +23,6 @@ import (
 
 	"knative.dev/eventing/pkg/reconciler/source"
 	"knative.dev/pkg/apis"
-	"knative.dev/pkg/kmeta"
 	servingv1 "knative.dev/serving/pkg/apis/serving/v1"
 
 	commonv1alpha1 "github.com/triggermesh/triggermesh/pkg/apis/common/v1alpha1"
@@ -105,9 +104,4 @@ func makeAppEnv(o *v1alpha1.InfraTarget) []corev1.EnvVar {
 	}
 
 	return env
-}
-
-// RBACOwners implements common.AdapterServiceBuilder.
-func (r *Reconciler) RBACOwners(trg commonv1alpha1.Reconcilable) ([]kmeta.OwnerRefable, error) {
-	return common.RBACOwners[*v1alpha1.InfraTarget](r.trgLister(trg.GetNamespace()))
 }
