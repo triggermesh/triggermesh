4469d2bb5799ee2e1d272fd7a6437fa4f56f6182
Antoine Cotten <hello@acotten.com>
2022-04-21 14:38:21 +0200

    Update AWSCognitoUserPoolSource

--- pkg/sources/reconciler/awscognitouserpoolsource/reconciler_test.go
+++ pkg/sources/reconciler/awscognitouserpoolsource/reconciler_test.go
@@ -51,10 +51,16 @@ func TestReconcileSource(t *testing.T) {
 func reconcilerCtor(cfg *adapterConfig) Ctor {
 	return func(t *testing.T, ctx context.Context, _ *rt.TableRow, ls *Listers) controller.Reconciler {
 		r := &Reconciler{
-			base:       NewTestDeploymentReconciler(ctx, ls),
 			adapterCfg: cfg,
-			srcLister:  ls.GetAWSCognitoUserPoolSourceLister().AWSCognitoUserPoolSources,
 		}
+
+		r.base = NewTestDeploymentReconciler(
+			ctx,
+			ls,
+			func(namespace string) common.Lister[*v1alpha1.AWSCognitoUserPoolSource] {
+				return ls.GetAWSCognitoUserPoolSourceLister().AWSCognitoUserPoolSources(namespace)
+			},
+		)
 
 		return reconcilerv1alpha1.NewReconciler(ctx, logging.FromContext(ctx),
 			fakeinjectionclient.Get(ctx), ls.GetAWSCognitoUserPoolSourceLister(),
