2b97dda07099948187dd776de880e05948d83760
Antoine Cotten <hello@acotten.com>
2022-04-21 17:24:49 +0200

    Update AzureQueueStorageSource

--- pkg/sources/reconciler/azurequeuestoragesource/adapter.go
+++ pkg/sources/reconciler/azurequeuestoragesource/adapter.go
@@ -22,7 +22,6 @@ import (
 
 	"knative.dev/eventing/pkg/reconciler/source"
 	"knative.dev/pkg/apis"
-	"knative.dev/pkg/kmeta"
 
 	commonv1alpha1 "github.com/triggermesh/triggermesh/pkg/apis/common/v1alpha1"
 	"github.com/triggermesh/triggermesh/pkg/apis/sources/v1alpha1"
@@ -59,9 +58,4 @@ func (r *Reconciler) BuildAdapter(src commonv1alpha1.Reconcilable, sinkURI *apis
 		resource.EnvVar(common.EnvName, src.GetName()),
 		resource.EnvVars(r.adapterCfg.configs.ToEnvVars()...),
 	)
-}
-
-// RBACOwners implements common.AdapterDeploymentBuilder.
-func (r *Reconciler) RBACOwners(src commonv1alpha1.Reconcilable) ([]kmeta.OwnerRefable, error) {
-	return common.RBACOwners[*v1alpha1.AzureQueueStorageSource](r.srcLister(src.GetNamespace()))
 }
