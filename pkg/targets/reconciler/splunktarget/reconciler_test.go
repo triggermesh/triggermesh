7c5d2dd4c23ca6a22e62bea7554a7566d890df63
Antoine Cotten <hello@acotten.com>
2022-04-21 18:15:40 +0200

    Update SplunkTarget

--- pkg/targets/reconciler/splunktarget/reconciler_test.go
+++ pkg/targets/reconciler/splunktarget/reconciler_test.go
@@ -50,10 +50,16 @@ func TestReconcile(t *testing.T) {
 func reconcilerCtor(cfg *adapterConfig) Ctor {
 	return func(t *testing.T, ctx context.Context, _ *rt.TableRow, ls *Listers) controller.Reconciler {
 		r := &Reconciler{
-			base:       NewTestServiceReconciler(ctx, ls),
 			adapterCfg: cfg,
-			trgLister:  ls.GetSplunkTargetLister().SplunkTargets,
 		}
+
+		r.base = NewTestServiceReconciler(
+			ctx,
+			ls,
+			func(namespace string) common.Lister[*v1alpha1.SplunkTarget] {
+				return ls.GetSplunkTargetLister().SplunkTargets(namespace)
+			},
+		)
 
 		return reconcilerv1alpha1.NewReconciler(ctx, logging.FromContext(ctx),
 			fakeinjectionclient.Get(ctx), ls.GetSplunkTargetLister(),
