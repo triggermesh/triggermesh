bb3f0f4bf4175cafd87b653f31de9b1fb7b8f8b5
Antoine Cotten <hello@acotten.com>
2022-04-21 18:11:18 +0200

    Update AWSDynamoDBTarget

--- pkg/targets/reconciler/awsdynamodbtarget/reconciler_test.go
+++ pkg/targets/reconciler/awsdynamodbtarget/reconciler_test.go
@@ -51,10 +51,16 @@ func TestReconcile(t *testing.T) {
 func reconcilerCtor(cfg *adapterConfig) Ctor {
 	return func(t *testing.T, ctx context.Context, _ *rt.TableRow, ls *Listers) controller.Reconciler {
 		r := &Reconciler{
-			base:       NewTestServiceReconciler(ctx, ls),
 			adapterCfg: cfg,
-			trgLister:  ls.GetAWSDynamoDBTargetLister().AWSDynamoDBTargets,
 		}
+
+		r.base = NewTestServiceReconciler(
+			ctx,
+			ls,
+			func(namespace string) common.Lister[*v1alpha1.AWSDynamoDBTarget] {
+				return ls.GetAWSDynamoDBTargetLister().AWSDynamoDBTargets(namespace)
+			},
+		)
 
 		return reconcilerv1alpha1.NewReconciler(ctx, logging.FromContext(ctx),
 			fakeinjectionclient.Get(ctx), ls.GetAWSDynamoDBTargetLister(),
