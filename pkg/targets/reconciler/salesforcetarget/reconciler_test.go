033f38307268538d674b3b4e8ac7b864feb538a8
Antoine Cotten <hello@acotten.com>
2022-04-21 18:15:14 +0200

    Update SalesforceTarget

--- pkg/targets/reconciler/salesforcetarget/reconciler_test.go
+++ pkg/targets/reconciler/salesforcetarget/reconciler_test.go
@@ -50,10 +50,16 @@ func TestReconcile(t *testing.T) {
 func reconcilerCtor(cfg *adapterConfig) Ctor {
 	return func(t *testing.T, ctx context.Context, _ *rt.TableRow, ls *Listers) controller.Reconciler {
 		r := &Reconciler{
-			base:       NewTestServiceReconciler(ctx, ls),
 			adapterCfg: cfg,
-			trgLister:  ls.GetSalesforceTargetLister().SalesforceTargets,
 		}
+
+		r.base = NewTestServiceReconciler(
+			ctx,
+			ls,
+			func(namespace string) common.Lister[*v1alpha1.SalesforceTarget] {
+				return ls.GetSalesforceTargetLister().SalesforceTargets(namespace)
+			},
+		)
 
 		return reconcilerv1alpha1.NewReconciler(ctx, logging.FromContext(ctx),
 			fakeinjectionclient.Get(ctx), ls.GetSalesforceTargetLister(),
