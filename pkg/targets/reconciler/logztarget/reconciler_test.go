d95535e3d7160a40b1791f9e716269501c756697
Antoine Cotten <hello@acotten.com>
2022-04-21 18:14:57 +0200

    Update LogzTarget

--- pkg/targets/reconciler/logztarget/reconciler_test.go
+++ pkg/targets/reconciler/logztarget/reconciler_test.go
@@ -49,10 +49,16 @@ func TestReconcile(t *testing.T) {
 func reconcilerCtor(cfg *adapterConfig) Ctor {
 	return func(t *testing.T, ctx context.Context, _ *rt.TableRow, ls *Listers) controller.Reconciler {
 		r := &Reconciler{
-			base:       NewTestServiceReconciler(ctx, ls),
 			adapterCfg: cfg,
-			trgLister:  ls.GetLogzTargetLister().LogzTargets,
 		}
+
+		r.base = NewTestServiceReconciler(
+			ctx,
+			ls,
+			func(namespace string) common.Lister[*v1alpha1.LogzTarget] {
+				return ls.GetLogzTargetLister().LogzTargets(namespace)
+			},
+		)
 
 		return reconcilerv1alpha1.NewReconciler(ctx, logging.FromContext(ctx),
 			fakeinjectionclient.Get(ctx), ls.GetLogzTargetLister(),
