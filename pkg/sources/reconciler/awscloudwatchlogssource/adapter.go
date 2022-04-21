1747a4cb85e835b7f48e683a109940f07dbb6d04
Antoine Cotten <hello@acotten.com>
2022-04-21 14:07:49 +0200

    Update AWSCloudWatchLogsSource

--- pkg/sources/reconciler/awscloudwatchlogssource/adapter.go
+++ pkg/sources/reconciler/awscloudwatchlogssource/adapter.go
@@ -23,7 +23,6 @@ import (
 
 	"knative.dev/eventing/pkg/reconciler/source"
 	"knative.dev/pkg/apis"
-	"knative.dev/pkg/kmeta"
 
 	commonv1alpha1 "github.com/triggermesh/triggermesh/pkg/apis/common/v1alpha1"
 	"github.com/triggermesh/triggermesh/pkg/apis/sources/v1alpha1"
@@ -70,9 +69,4 @@ func (r *Reconciler) BuildAdapter(src commonv1alpha1.Reconcilable, sinkURI *apis
 		resource.Port(healthPortName, 8080),
 		resource.StartupProbe("/health", healthPortName),
 	)
-}
-
-// RBACOwners implements common.AdapterDeploymentBuilder.
-func (r *Reconciler) RBACOwners(src commonv1alpha1.Reconcilable) ([]kmeta.OwnerRefable, error) {
-	return common.RBACOwners[*v1alpha1.AWSCloudWatchLogsSource](r.srcLister(src.GetNamespace()))
 }
