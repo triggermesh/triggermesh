6a82f7852b8a5a4c1617b0b784077b51e8c698b0
Antoine Cotten <hello@acotten.com>
2022-04-21 18:13:02 +0200

    Update ElasticsearchTarget

--- pkg/targets/reconciler/elasticsearchtarget/reconciler_test.go
+++ pkg/targets/reconciler/elasticsearchtarget/reconciler_test.go
@@ -49,10 +49,16 @@ func TestReconcile(t *testing.T) {
 func reconcilerCtor(cfg *adapterConfig) Ctor {
 	return func(t *testing.T, ctx context.Context, _ *rt.TableRow, ls *Listers) controller.Reconciler {
 		r := &Reconciler{
-			base:       NewTestServiceReconciler(ctx, ls),
 			adapterCfg: cfg,
-			trgLister:  ls.GetElasticsearchTargetLister().ElasticsearchTargets,
 		}
+
+		r.base = NewTestServiceReconciler(
+			ctx,
+			ls,
+			func(namespace string) common.Lister[*v1alpha1.ElasticsearchTarget] {
+				return ls.GetElasticsearchTargetLister().ElasticsearchTargets(namespace)
+			},
+		)
 
 		return reconcilerv1alpha1.NewReconciler(ctx, logging.FromContext(ctx),
 			fakeinjectionclient.Get(ctx), ls.GetElasticsearchTargetLister(),
