8f10cfaeeef0c5cc9667319a2e5cbfb2b36d7f5a
Antoine Cotten <hello@acotten.com>
2022-04-21 18:11:39 +0200

    Update AWSKinesisTarget

--- pkg/targets/reconciler/awskinesistarget/reconciler_test.go
+++ pkg/targets/reconciler/awskinesistarget/reconciler_test.go
@@ -51,10 +51,16 @@ func TestReconcile(t *testing.T) {
 func reconcilerCtor(cfg *adapterConfig) Ctor {
 	return func(t *testing.T, ctx context.Context, _ *rt.TableRow, ls *Listers) controller.Reconciler {
 		r := &Reconciler{
-			base:       NewTestServiceReconciler(ctx, ls),
 			adapterCfg: cfg,
-			trgLister:  ls.GetAWSKinesisTargetLister().AWSKinesisTargets,
 		}
+
+		r.base = NewTestServiceReconciler(
+			ctx,
+			ls,
+			func(namespace string) common.Lister[*v1alpha1.AWSKinesisTarget] {
+				return ls.GetAWSKinesisTargetLister().AWSKinesisTargets(namespace)
+			},
+		)
 
 		return reconcilerv1alpha1.NewReconciler(ctx, logging.FromContext(ctx),
 			fakeinjectionclient.Get(ctx), ls.GetAWSKinesisTargetLister(),
