b2171c6c7f6e2d6012b4ffe652616751cc469447
Antoine Cotten <hello@acotten.com>
2022-04-21 18:45:58 +0200

    Update JQTransformation

--- pkg/flow/reconciler/jqtransformation/adapter.go
+++ pkg/flow/reconciler/jqtransformation/adapter.go
@@ -21,7 +21,6 @@ import (
 
 	"knative.dev/eventing/pkg/reconciler/source"
 	"knative.dev/pkg/apis"
-	"knative.dev/pkg/kmeta"
 	servingv1 "knative.dev/serving/pkg/apis/serving/v1"
 
 	commonv1alpha1 "github.com/triggermesh/triggermesh/pkg/apis/common/v1alpha1"
@@ -78,9 +77,4 @@ func makeAppEnv(o *v1alpha1.JQTransformation) []corev1.EnvVar {
 	}
 
 	return env
-}
-
-// RBACOwners implements common.AdapterServiceBuilder.
-func (r *Reconciler) RBACOwners(trg commonv1alpha1.Reconcilable) ([]kmeta.OwnerRefable, error) {
-	return common.RBACOwners[*v1alpha1.JQTransformation](r.trgLister(trg.GetNamespace()))
 }
