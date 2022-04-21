9aa934ac619ed9c74095305663b5b12c9fa080e4
Antoine Cotten <hello@acotten.com>
2022-04-21 18:15:49 +0200

    Update TektonTarget

--- pkg/targets/reconciler/tektontarget/reconciler.go
+++ pkg/targets/reconciler/tektontarget/reconciler.go
@@ -30,7 +30,7 @@ import (
 
 // Reconciler implements controller.Reconciler for the event target type.
 type Reconciler struct {
-	base       common.GenericServiceReconciler
+	base       common.GenericServiceReconciler[*v1alpha1.TektonTarget]
 	adapterCfg *adapterConfig
 
 	trgLister func(namespace string) listersv1alpha1.TektonTargetNamespaceLister
