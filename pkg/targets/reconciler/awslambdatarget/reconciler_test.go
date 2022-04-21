d8912e858ead5c37f009a54a0942bbca4994f4e2
Antoine Cotten <hello@acotten.com>
2022-04-21 18:11:51 +0200

    Update AWSLambdaTarget

--- pkg/targets/reconciler/awslambdatarget/reconciler_test.go
+++ pkg/targets/reconciler/awslambdatarget/reconciler_test.go
@@ -51,10 +51,16 @@ func TestReconcile(t *testing.T) {
 func reconcilerCtor(cfg *adapterConfig) Ctor {
 	return func(t *testing.T, ctx context.Context, _ *rt.TableRow, ls *Listers) controller.Reconciler {
 		r := &Reconciler{
-			base:       NewTestServiceReconciler(ctx, ls),
 			adapterCfg: cfg,
-			trgLister:  ls.GetAWSLambdaTargetLister().AWSLambdaTargets,
 		}
+
+		r.base = NewTestServiceReconciler(
+			ctx,
+			ls,
+			func(namespace string) common.Lister[*v1alpha1.AWSLambdaTarget] {
+				return ls.GetAWSLambdaTargetLister().AWSLambdaTargets(namespace)
+			},
+		)
 
 		return reconcilerv1alpha1.NewReconciler(ctx, logging.FromContext(ctx),
 			fakeinjectionclient.Get(ctx), ls.GetAWSLambdaTargetLister(),
