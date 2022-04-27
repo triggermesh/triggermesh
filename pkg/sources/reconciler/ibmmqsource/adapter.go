e076d967e7304017c692dc15388a9956cb0c0c7c
Antoine Cotten <hello@acotten.com>
2022-04-27 07:50:49 +0200

    feat: Label IBM MQ source metrics with name and namespace (#799)

--- pkg/sources/reconciler/ibmmqsource/adapter.go
+++ pkg/sources/reconciler/ibmmqsource/adapter.go
@@ -89,6 +89,8 @@ func (r *Reconciler) BuildAdapter(src commonv1alpha1.Reconcilable, sinkURI *apis
 		resource.Image(r.adapterCfg.Image),
 
 		resource.EnvVars(makeAppEnv(typedSrc)...),
+		resource.EnvVar(common.EnvNamespace, src.GetNamespace()),
+		resource.EnvVar(common.EnvName, src.GetName()),
 		resource.EnvVars(r.adapterCfg.configs.ToEnvVars()...),
 
 		keystoreMount,
