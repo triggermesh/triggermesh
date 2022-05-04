3721d7ec3495ba5121c75388b1130aa5d58de788
Pablo Mercado <pablo@triggermesh.com>
2022-05-04 16:37:57 +0200

    Update pkg/targets/reconciler/cloudeventstarget/adapter.go
    Co-authored-by: Antoine Cotten <hello@acotten.com>

--- pkg/targets/reconciler/cloudeventstarget/adapter.go
+++ pkg/targets/reconciler/cloudeventstarget/adapter.go
@@ -83,7 +83,7 @@ func (r *Reconciler) BuildAdapter(trg commonv1alpha1.Reconcilable, _ *apis.URL) 
 	options = append(options,
 		resource.EnvVars(makeAppEnv(typedTrg)...),
 		// make sure that a non optional parameter is located as the last element
-		// to avoid derivative compoarisons issues when the environemnt variables
+		// to avoid derivative comparison issues when the environment variables
 		// tail element is removed.
 		resource.Image(r.adapterCfg.Image),
 		resource.EnvVars(r.adapterCfg.obsConfig.ToEnvVars()...),
