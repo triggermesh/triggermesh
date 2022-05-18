f675323fe7a78d478c87f73704ccac775774e10b
Antoine Cotten <hello@acotten.com>
2022-05-18 11:51:58 +0200

    feat(src/sqs): Let CPU limit dictate number of processors (#910)
    Revert to letting uber-go/automaxprocs determine the ideal GOMAXPROCS
    value, which can be influenced by setting a CPU limit manually.

--- cmd/awssqssource-adapter/main.go
+++ cmd/awssqssource-adapter/main.go
@@ -17,30 +17,11 @@ limitations under the License.
 package main
 
 import (
-	"runtime"
-
 	"knative.dev/eventing/pkg/adapter/v2"
 
 	"github.com/triggermesh/triggermesh/pkg/sources/adapter/awssqssource"
 )
 
 func main() {
-	setMaxProcs(runtime.NumCPU())
-
 	adapter.Main("awssqssource", awssqssource.NewEnvConfig, awssqssource.NewAdapter)
-}
-
-// setMaxProcs sets the number of threads that can be used by the current
-// process.
-//
-// Knative uses uber-go/automaxprocs to automatically determine the number of
-// threads (GOMAXPROCS) available to the process based on CPU quotas (e.g.
-// 'cpu.request <= 1' translates to 1 thread, regardless of the number of
-// physical cores).
-// This event source has a very predictable CPU profile: it spends most of its
-// time sending network requests without performing any computation on the
-// results, so we assume the defined CPU limit allows all CPU cores to be used
-// without starving on CPU time.
-func setMaxProcs(procs int) int {
-	return runtime.GOMAXPROCS(procs)
 }
