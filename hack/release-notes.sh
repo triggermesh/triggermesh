9f0bdfe300dfbf6daf5fe67f7581cdef561ede44
Sameer Naik <sameer@fuloi.com>
2022-05-31 20:15:41 +0530

    release-notes.sh: read release highlights from tag annotation

--- hack/release-notes.sh
+++ hack/release-notes.sh
@@ -18,6 +18,7 @@ KREPO="triggermesh"
 BASE_URL="https://github.com/triggermesh/${KREPO}/releases/download/${RELEASE}"
 PREV_RELEASE=${PREV_RELEASE:-$(git describe --tags --abbrev=0 ${RELEASE}^ 2>/dev/null)}
 PREV_RELEASE=${PREV_RELEASE:-$(git rev-list --max-parents=0 ${RELEASE}^ 2>/dev/null)}
+NOTABLE_CHANGES=$(git cat-file -p ${RELEASE} | sed '/-----BEGIN PGP SIGNATURE-----/,//d' | tail -n +6)
 CHANGELOG=$(git log --no-merges --pretty=format:'- [%h] %s (%aN)' ${PREV_RELEASE}..${RELEASE})
 if [ $? -ne 0 ]; then
   echo "Error creating changelog"
@@ -37,6 +38,8 @@ RELEASE_ASSETS_TABLE=$(
 )
 
 cat <<EOF
+${NOTABLE_CHANGES}
+
 ## Installation
 
 Install all TriggerMesh custom resources:
