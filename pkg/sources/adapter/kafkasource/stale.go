773f1ea4da52e3d5c5efac7a5d2d1a141854c8e7
odacremolbap <pablo@triggermesh.com>
2023-02-16 15:06:12 +0100

    make linter happy

--- pkg/sources/adapter/kafkasource/stale.go
+++ pkg/sources/adapter/kafkasource/stale.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2023 TriggerMesh Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package kafkasource
 
 import (
@@ -44,7 +60,7 @@ func (sl *StaleList) count() int {
 	return len(sl.items)
 }
 
-// Adds new element to the list and updates the count, removing
+// AddAndCount adds a new element to the list and updates the count, removing
 // any stale items from it.
 func (sl *StaleList) AddAndCount(object interface{}) int {
 	sl.m.Lock()
@@ -58,7 +74,7 @@ func (sl *StaleList) AddAndCount(object interface{}) int {
 	return sl.count()
 }
 
-// Updates the count removing any stale items from it.
+// Count updates the count removing any stale items from it.
 func (sl *StaleList) Count() int {
 	sl.m.Lock()
 	defer sl.m.Unlock()
