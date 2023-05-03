a42dfeeab086766c547605e32146559d5cce5991
Fran Barrera <fran@triggermesh.com>
2023-05-03 13:24:57 +0200

    CloudEventsTarget - Remove sensitive information (#1411)
    * CloudEventsTarget - Fix gh security
    
    * Fix typo
    
    * remove unnecessary fmt

--- pkg/targets/adapter/cloudeventstarget/adapter.go
+++ pkg/targets/adapter/cloudeventstarget/adapter.go
@@ -73,7 +73,7 @@ func NewTarget(ctx context.Context, envAcc pkgadapter.EnvConfigAccessor, listenC
 
 		if err := fw.Add(env.BasicAuthPasswordPath, ceClientUpdater); err != nil {
 			logger.Panicw(
-				fmt.Sprintf("Authentication secret at %q could not be watched", env.BasicAuthPasswordPath),
+				"Authentication secret could not be watched at the specific path",
 				zap.Error(err))
 		}
 		ceAdapter.fileWatcher = fw
@@ -113,7 +113,7 @@ func (a *ceAdapter) senderClientUpdater(url, path, username string) fs.WatchCall
 		if username != "" {
 			password, err := os.ReadFile(path)
 			if err != nil {
-				a.logger.Errorw("Could not read the mounted password.", zap.Error(err), zap.String("path", path))
+				a.logger.Errorw("Could not read the mounted password at the specific path", zap.Error(err))
 				return
 			}
 
